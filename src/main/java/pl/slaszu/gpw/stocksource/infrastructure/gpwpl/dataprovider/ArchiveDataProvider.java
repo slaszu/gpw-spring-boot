c4198fe5bf7d8af9f26fbd3228903767c812b01c
Piotr Flasza <piotr.flasza@enp.pl>
2023-02-23 16:28:48 +0100

    gpw.pl archival works | todo: finish

--- src/main/java/pl/slaszu/gpw/stocksource/infrastructure/gpwpl/dataprovider/ArchiveDataProvider.java
+++ src/main/java/pl/slaszu/gpw/stocksource/infrastructure/gpwpl/dataprovider/ArchiveDataProvider.java
@@ -1,8 +1,12 @@
 package pl.slaszu.gpw.stocksource.infrastructure.gpwpl.dataprovider;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import pl.slaszu.gpw.stocksource.application.FetchStocks.DataProviderInterface;
@@ -10,11 +14,12 @@ import pl.slaszu.gpw.stocksource.application.FetchStocks.FetchStocksException;
 import pl.slaszu.gpw.stocksource.application.FetchStocks.StockDto;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.net.URL;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -23,10 +28,17 @@ public class ArchiveDataProvider implements DataProviderInterface {
 
     private String url;
 
-    public ArchiveDataProvider(@Value("${gpw.gpwpl.url-archive}") String url) {
+    private String dirTemp;
+
+    public ArchiveDataProvider(
+            @Value("${gpw.gpwpl.url-archive}") String url,
+            @Value("${gpw.gpwpl.dir-temp}") String dirTemp
+    ) {
         this.url = url;
+        this.dirTemp = dirTemp;
     }
 
+    @SneakyThrows
     @Override
     public List<StockDto> getData(Date date) throws FetchStocksException {
         // prepare url
@@ -36,20 +48,45 @@ public class ArchiveDataProvider implements DataProviderInterface {
         log.debug("Gpw.pl archive date '%s'".formatted(format.format(date)));
         log.debug("Gpw.pl archive url '%s'".formatted(currentUrl));
 
-        // TODO: 21.02.2023 download file first 
-        FileInputStream file = null;
-        try {
-            file = new FileInputStream(new File(currentUrl));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            Workbook workbook = new XSSFWorkbook(file);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        // TODO: 23.02.2023 copyURLToFile can be removed 
+//        File file = new File(this.dirTemp, "last_archive.xls");
+//        FileUtils.copyURLToFile(new URL(currentUrl), file, 3000, 3000);
+
+        Workbook workbook = new HSSFWorkbook(new URL(currentUrl).openStream());
+        Sheet sheet = workbook.getSheetAt(0);
+        Iterator<Row> iterator = sheet.rowIterator();
+
+        List<StockDto> result = new ArrayList<>();
+        // skip first row
+        iterator.next();
+        while (iterator.hasNext()) {
+            Row row = iterator.next();
+
+            result.add(this.fromRow(row));
         }
 
-        return null;
+
+        return result;
+    }
+
+    private StockDto fromRow(Row row) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date date = formatter.parse(row.getCell(0).getStringCellValue());
+
+        // TODO: 23.02.2023 code in stock is not good, remove and use only name 
+        
+        return new StockDto(
+                row.getCell(2).getStringCellValue(), // code
+                row.getCell(1).getStringCellValue(), // name
+                (float) row.getCell(4).getNumericCellValue(), // open
+                (float) row.getCell(5).getNumericCellValue(), // high
+                (float) row.getCell(6).getNumericCellValue(), // low
+                (float) row.getCell(7).getNumericCellValue(), // price
+                (int) row.getCell(9).getNumericCellValue(), // volumen
+                (int) row.getCell(10).getNumericCellValue(), // amount
+                date
+        );
     }
 
 
