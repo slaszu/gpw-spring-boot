9807a6524afea55f6814591b66227ce4fb87218f
slaszu <piotr.flasza@gmail.com>
2023-03-13 20:54:12 +0100

    mysql

--- src/main/java/pl/slaszu/gpw/stocksource/infrastructure/stooq/model/Header.java
+++ src/main/java/pl/slaszu/gpw/stocksource/infrastructure/stooq/model/Header.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Header {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     @Column(name = "id", nullable = false)
     private Long id;
 
