8ee7cf97977a3e7f7e4fb41e0b1e8f15c8ec8962
Piotr Flasza <piotr.flasza@enp.pl>
2023-03-16 13:26:52 +0100

    intergation tests works

--- src/main/java/pl/slaszu/gpw/stock/infrastructure/rest/StockController.java
+++ src/main/java/pl/slaszu/gpw/stock/infrastructure/rest/StockController.java
@@ -5,22 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.slaszu.gpw.stock.application.CreateStock.CreateStockService;
 import pl.slaszu.gpw.stock.application.ListStocks.ListStocksQuery;
 import pl.slaszu.gpw.stock.application.ListStocks.ListStocksService;
 import pl.slaszu.gpw.stock.application.ListStocks.StockViewModel;
 
 import java.util.List;
 
-// TODO: 27.01.2023 add stock with price endpoint /stocks/{code}/{date|optional} 
-
 @RestController
 @RequestMapping("/stocks")
 @Tag(name = "core")
 public class StockController {
-
-    @Autowired
-    private CreateStockService createStockService;
 
     @Autowired
     private ListStocksService listStocksService;
--- src/test/java/pl/slaszu/gpw/integration/StockRestControllerIntegrationTest.java
+++ src/test/java/pl/slaszu/gpw/integration/StockRestControllerIntegrationTest.java
@@ -4,13 +4,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import pl.slaszu.gpw.stock.application.ListStocks.ListStocksService;
+import pl.slaszu.gpw.stock.application.ListStocks.StockViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,13 +27,33 @@ public class StockRestControllerIntegrationTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private ListStocksService service;
+
     @Test
-    public void givenEmployees_whenGetEmployees_thenStatus200()
-            throws Exception {
+    public void endpointStocksExists_returnEmptyArray() throws Exception {
 
         mvc.perform(get("/stocks")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("[]"));
+    }
+
+    @Test
+    public void endpointStocksExists_returnGivenResults() throws Exception {
+
+        List<StockViewModel> result = new ArrayList<>();
+        result.add(new StockViewModel("uuid_123", "Stock_code_123"));
+
+        when(service.query(any())).thenReturn(result);
+
+        mvc.perform(get("/stocks")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$.[0].id").value("uuid_123"))
+                .andExpect(jsonPath("$.[0].code").value("Stock_code_123"));
     }
 }
