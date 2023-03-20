5a7e5e410563772a6c8a9f06030896fff2001a5f
slaszu <piotr.flasza@gmail.com>
2023-03-20 21:59:01 +0100

    todo for future

--- src/main/java/pl/slaszu/gpw/GpwApplication.java
+++ src/main/java/pl/slaszu/gpw/GpwApplication.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+// TODO: 20/03/2023 logging to file with rotate log, logging only excption in PROD profile
+// TODO: 20/03/2023 create PROD profile 
+// TODO: 20/03/2023 add last_updated date to product_stock 
+// TODO: 20/03/2023 create endoints form stocks and stocks_price with filtering 
+
 @SpringBootApplication
 @ComponentScan(nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class)
 @EnableScheduling
