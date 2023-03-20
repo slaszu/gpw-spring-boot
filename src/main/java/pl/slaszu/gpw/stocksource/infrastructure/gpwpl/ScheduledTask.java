b67b43957661ab7695655fa239ac65474f348522
slaszu <piotr.flasza@gmail.com>
2023-03-20 21:56:24 +0100

    schedler

--- src/main/java/pl/slaszu/gpw/stocksource/infrastructure/gpwpl/ScheduledTask.java
+++ src/main/java/pl/slaszu/gpw/stocksource/infrastructure/gpwpl/ScheduledTask.java
@@ -1,8 +1,11 @@
 package pl.slaszu.gpw.stocksource.infrastructure.gpwpl;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import pl.slaszu.gpw.stocksource.infrastructure.gpwpl.dataprovider.TodayDataProvider;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -12,8 +15,16 @@ import java.util.Date;
 public class ScheduledTask {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 1000)
-    public void reportCurrentTime() {
-        log.info("The time is now {}", dateFormat.format(new Date()));
+    @Autowired
+    private TodayDataProvider todayDataProvider;
+
+    @SneakyThrows
+    @Scheduled(cron = "0 */15 10-17 * * 0-5 ")
+    @Scheduled(cron = "0 15,30,40 9 * * 0-5")
+    public void getCurrentStock() {
+        Date date = new Date();
+
+        log.info("Scheduler {}", dateFormat.format(date));
+        todayDataProvider.getData(date);
     }
 }
