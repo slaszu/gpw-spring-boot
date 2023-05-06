ed5ae1b933d12f978ef55a27d6388fd32d9648f1
slaszu <piotr.flasza@gmail.com>
2023-05-06 19:02:01 +0200

    set pl as default locale for jollyday

--- src/main/java/pl/slaszu/gpw/calendar/Config.java
+++ src/main/java/pl/slaszu/gpw/calendar/Config.java
@@ -1,6 +1,8 @@
 package pl.slaszu.gpw.calendar;
 
 import de.focus_shift.HolidayManager;
+import de.focus_shift.ManagerParameter;
+import de.focus_shift.ManagerParameters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +11,6 @@ public class Config {
 
     @Bean
     public HolidayManager getHolidayManager() {
-        return HolidayManager.getInstance();
+        return HolidayManager.getInstance(ManagerParameters.create("pl"));
     }
 }
